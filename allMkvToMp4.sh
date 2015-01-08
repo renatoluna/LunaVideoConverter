3ba95c1308bb2432b29988b8a02e5899023a1c8d
Renato Luna <renatoluna@gmail.com>
2015-01-08 21:39:10 -0200

    Update allMkvToMp4.sh

--- allMkvToMp4.sh
+++ allMkvToMp4.sh
@@ -1,4 +1,4 @@
 mkdir mp4
 for i in *.mkv; do
-    ffmpeg -i "$i" -vcodec copy -acodec copy -metadata show="The Office" "mp4/${i//\.mkv/.mp4}"
+    ffmpeg -i "$i" -vcodec copy -acodec copy -metadata show="The Office (US)" "mp4/${i//\.mkv/.mp4}"
 done
